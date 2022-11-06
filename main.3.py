8dc07a20e1ca8fa626f349f9ac55ecc220819c6d
OlexandrDemch <sdemcuk811@gmail.com>
2022-11-05 21:02:17 -1200

    py_app_example_2

---
+++ main.3.py
@@ -0,0 +1,9 @@
+m = float(input('m: '))
+
+print(f'{m} mil = {0.000621371 * m} mil')
+
+print(f'{m} m = {10 * m} dm')
+
+print(f'{m} sm = {100 * m} sm')
+
+print(f'{m} sm = {1000 * m} mm')
---
+++ main.4.py
@@ -0,0 +1,5 @@
+s1 = int(input('s1->'))
+s2 = int(input('s2->'))
+s3 = int(input('s3->'))
+res = (s1+s2)*s3
+print(f'{s1+s2+s3} = {res}')
